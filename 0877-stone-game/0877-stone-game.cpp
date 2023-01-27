8c182d78352b0bf46589b2d12e6a1b70fe8784bd
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-01-27 15:36:20 +0530

    Time: 17 ms (39.65%), Space: 9.8 MB (33.88%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -8,7 +8,7 @@ public:
             return dp[i][j][a]=max(v[i]+f(i+1,j,v,0),v[j]+f(i,j-1,v,0));
         }
         else 
-            return dp[i][j][a]=max(f(i+1,j,v,1),f(i,j-1,v,1));
+            return dp[i][j][a]=min(f(i+1,j,v,1),f(i,j-1,v,1));
     }
     bool stoneGame(vector<int>& piles) {
         memset(dp,-1,sizeof(dp));
