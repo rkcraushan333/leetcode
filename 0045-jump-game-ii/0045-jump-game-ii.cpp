4c6dbbd01bf1ad8c2b0e92c5ef4f2e0506d1d48a
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-02-08 12:11:57 +0530

    Time: 350 ms (33.06%), Space: 17.9 MB (14.62%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[10001];
+    int f(vector<int>&v,int i=0){
+        if(i==v.size()-1) return 0;
+        int res = 1e8;
+        if(dp[i]!=-1) return dp[i];
+        for(int j=1;j<=v[i];j++){
+            if(i+j>=v.size()) break;
+            res = min(res,1+f(v,i+j));
+        }
+        return dp[i]=res;
+    }
+    int jump(vector<int>& nums) {
+        memset(dp,-1,sizeof(dp));
+        return f(nums);
+    }
+};
