844eb859f10c4849a007ebd2ff16e1da9be776b2
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-02-09 01:42:13 +0530

    Time: 360 ms (37.72%), Space: 110.6 MB (19.55%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minJumps(vector<int>& arr) {
+        map<int,vector<int>>adj;
+        for(int i=0;i<arr.size();i++){
+            adj[arr[i]].push_back(i);
+        }
+        vector<int>vis(arr.size());
+        queue<pair<int,int>>q;
+        q.push({0,0});
+        vis[0] = 1;
+        while(!q.empty()){
+            int i = q.front().first;
+            int t = q.front().second;
+            q.pop();
+            if(i==arr.size()-1) return t;
+            for(auto &x:adj[arr[i]]){
+                if(!vis[x]) {q.push({x,t+1});vis[x]=1;}
+            }
+            if(i<arr.size()-1 && vis[i+1]==0) {q.push({i+1,t+1});vis[i+1]=1;}
+            if(i>0 && vis[i-1]==0) {q.push({i-1,t+1});vis[i-1]=1;}
+            adj.erase(arr[i]);
+        }
+        return 0;
+     }
+};
