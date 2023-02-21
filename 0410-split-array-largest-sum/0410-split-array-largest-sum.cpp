49a79056abac6493e9039bab19eb4ba0553037fe
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-02-22 05:11:55 +0530

    Time: 464 ms (13.85%), Space: 7.6 MB (20.92%) - LeetHub

---
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int dp[1001][51];
+    int f(int k,vector<int>&v,int i=0){
+        if(k==0 && i==v.size()) return 0;
+        if(k==0||i==v.size()) return INT_MAX;
+        int &t = dp[i][k];
+        if(t!=-1) return t; 
+        int res = INT_MAX;
+        int s=0;
+        for(int j=i;j<v.size();j++){
+            s+=v[j];
+            res = min(res,max(s,f(k-1,v,j+1)));
+        }
+        return t=res;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        memset(dp,-1,sizeof(dp));
+        return f(k,nums); 
+    }
+};
