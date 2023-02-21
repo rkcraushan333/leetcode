b05155241cf61ec6093dfa4fb0f33100f9201cec
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-02-22 00:00:21 +0530

    Time: 278 ms (62.04%), Space: 85.7 MB (61.74%) - LeetHub

---
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[100001][3][2];
+    int f(int k,vector<int>&p,int i=0,bool b = 1){
+        if(k==0 || i==p.size()) return  0;
+        int &t = dp[i][k][b];
+        if(t!=-1) return t;
+        int res = 0;
+        if(b){
+            res = max(res,(-1*p[i])+f(k,p,i+1,0));
+            res = max(res,f(k,p,i+1,1));
+        }
+        else{
+            res = max(res,p[i]+f(k-1,p,i+1,1));
+            res = max(res,f(k,p,i+1,0));
+        }
+        return  t=res;
+    }
+    int maxProfit(vector<int>& prices) {
+        memset(dp,-1,sizeof(dp));
+        return f(2,prices);
+    }
+};
