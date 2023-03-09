8d58f66feb128f5d4093027446eae03063cbc40a
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-03-09 13:22:05 +0530

    Time: 4 ms (95.66%), Space: 7.7 MB (68.59%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -1,13 +1,27 @@
+// x1 + x2 +x3 + x2 , x1+x2 :- x1 = x3 as slow = fast/2
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        unordered_set<ListNode*>s;
-        while(head){
-            if(s.find(head)!=s.end())
-                return head;
-            s.insert(head);
-            head = head->next;
+        if(!head)
+            return NULL;
+        if(!head->next) return NULL;
+        ListNode* slow = head;
+        ListNode* fast = head;
+        bool check = 0;
+        while(fast->next && fast->next->next){
+            slow = slow->next;
+            fast = fast->next->next;
+            if(slow==fast) {
+                check = 1;
+                break;
+            }
         }
-        return NULL;
+        if(!check) return NULL;
+        slow = head;
+        while(slow!=fast){
+            slow=slow->next;
+            fast = fast->next;
+        }
+        return slow;
     }
 };
