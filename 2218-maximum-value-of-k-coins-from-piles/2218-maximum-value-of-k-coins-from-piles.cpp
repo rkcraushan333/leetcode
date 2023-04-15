fab5b92b54f1a3ee380d2c01d0a22824efb46091
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-04-15 11:07:21 +0530

    Time: 303 ms (54.45%), Space: 19.7 MB (12.83%) - LeetHub

--- 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
+++ 2218-maximum-value-of-k-coins-from-piles/2218-maximum-value-of-k-coins-from-piles.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int dp[1001][2001];
-    int f(vector<vector<int>>&v,int k,int i=0){
+    int f(vector<vector<int>>&piles,int k,int i=0){
         if(k==0) return 0;
-        if(i==v.size()) return INT_MIN;
+        if(i==piles.size()||k<0) return INT_MIN;
         int &t = dp[i][k];
         if(t!=-1) return t;
-        int res = INT_MIN;
-        res = max(res,f(v,k,i+1));
-        int curr = 0;
-        for(int idx=0;idx<v[i].size();idx++){
-            if(idx+1>k) break;
-            curr += v[i][idx];
-           res = max(res,curr+f(v,k-idx-1,i+1));
-        }        
-        return t = res;
+        int ans = 0,sum = 0;
+        ans = max(ans,f(piles,k,i+1));
+        for(int j=0;j<piles[i].size();j++){
+            sum += piles[i][j];
+            ans = max(ans,sum+f(piles,k-(j+1),i+1));
+        }
+        return t = ans;
     }
     int maxValueOfCoins(vector<vector<int>>& piles, int k) {
         memset(dp,-1,sizeof(dp));
