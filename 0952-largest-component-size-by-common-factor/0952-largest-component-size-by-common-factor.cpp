a749f3b9bdda763846e94ff5a5957578338abad5
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-04-21 23:29:36 +0530

    Time: 2727 ms (5.18%), Space: 311.3 MB (5.18%) - LeetHub

--- 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.cpp
+++ 0952-largest-component-size-by-common-factor/0952-largest-component-size-by-common-factor.cpp
@@ -1,40 +1,47 @@
 class Solution {
 public:
-    int par[100001];
-    int size[100001];
-    int find(int a){
-        if(par[a]==a) return a;
-        return par[a] = find(par[a]);
+    vector<int> par;
+    vector<int> Size;
+    void make_Set(int v)
+    {
+        par[v] = v;
+        Size[v] = 1;
     }
-    void unionset(int a,int b){
-        a = find(a);
-        b = find(b);
-        if(a!=b){
-            if(size[a]<size[b]) swap(a,b);
-            par[b] = a;
-            size[a] += size[b];
-        }
+    int find_Set(int v)
+    {
+        return par[v] = (par[v] == v) ? v : find_Set(par[v]);
+    }
+    void union_Set(int u, int v)
+    {
+        u = find_Set(u);
+        v = find_Set(v);
+        if (u != v){
+            if(Size[u]<Size[v]) swap(u,v);
+            par[v] = u;
+            Size[u] += Size[v];}
     }
     int largestComponentSize(vector<int>& nums) {
-        for(int i=0;i<100000;i++) {par[i] = i;size[i]=1;}
+        par.resize(1e6);
+        Size.resize(1e6);
+        for(int i=0;i<1e6;i++) make_Set(i);
         unordered_set<int>s;
         int n = 0;
-        for(int i=0;i<nums.size();i++) {s.insert(nums[i]); n= max(n,nums[i]);}
+        for(int i=0;i<nums.size();i++){ s.insert(nums[i]); n = max(n,nums[i]);}
         for(int i=2;i<=n;i++){
             int last = -1;
-            for(int j=i;j<=n;j+=i){
+            for(int j = i;j<=n;j+=i){
                 if(s.find(j)!=s.end()){
-                    if(last==-1){
+                    if(last==-1) last = j;
+                    else{
+                        union_Set(last,j);
                         last = j;
-                    }else{
-                        unionset(last,j);
                     }
                 }
             }
         }
-        int res = 1;
-        for(auto &i:s){
-            res = max(res,size[i]);           
+        int res = 0;
+        for(auto i:Size){
+            res = max(i,res);
         }
         return res;
     }
