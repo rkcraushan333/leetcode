fe1b7967c64285c36b841b30c0abfba9c84098c4
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-04-22 12:05:31 +0530

    Time: 52 ms (75.58%), Space: 7.5 MB (85.54%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,21 +1,22 @@
 // length - Longest palindromic subsequence
 class Solution {
 public:
-    int dp[501][501];
-    int f(string &s,string &t,int i=0,int j=0){
-        if(i==s.size()||j==s.size()) return 0;
-        int &tt = dp[i][j];
-        if(tt!=-1) return tt;
-        int ans =0;
-        ans = max(ans,f(s,t,i,j+1));
-        ans = max(ans,f(s,t,i+1,j));
-        if(s[i]==t[j]) ans = max(ans,1+f(s,t,i+1,j+1));
-        return tt=ans;
-    }
     int minInsertions(string s) {
-        memset(dp,-1,sizeof(dp));
         string t = s;
         reverse(t.begin(),t.end());
-        return s.size()-f(s,t);
+        int n = s.size();
+        int dp[n+1][n+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=n;j++)
+                dp[i][j] = 0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+                dp[i][j] = max(dp[i-1][j],dp[i][j]);
+                dp[i][j] = max(dp[i][j-1],dp[i][j]);
+                if(s[i-1]==t[j-1]) dp[i][j] = max(dp[i][j],1+dp[i-1][j-1]);
+            }
+        }
+        return s.size()-dp[n][n];
     }
 };
