a0e40bfb03b5441aa81bcb278cf4818ed9add321
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-06-22 01:01:03 +0530

    Time: 79 ms (99.27%), Space: 45 MB (19.34%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.cpp
@@ -1,35 +1,36 @@
-// Using Binary Search
+// using prefix sum
 class Solution {
 public:
     #define ll long long
-    ll f(int m,vector<int>&nums,vector<int>&cost)
+    long long minCost(vector<int>& nums, vector<int>& cost) 
     {
-        ll ans = 0;
+        vector<pair<ll,ll>>v;
         int n = nums.size();
         for(int i=0;i<n;i++)
         {
-            ans += abs(nums[i]-m)*1ll*cost[i];
+            v.push_back({nums[i],cost[i]});
         }
-        return ans;
-    }
-    long long minCost(vector<int>& nums, vector<int>& cost) {
-        int s = INT_MAX;
-        int e = INT_MIN;
-        for(auto i:nums)
+        sort(v.begin(),v.end());
+        ll ans = 0;
+        // finding ans for the 1st index
+        for(int i=1;i<n;i++)
         {
-            s = min(s,i); e = max(e,i);
+            ans += (v[i].first-v[0].first)*v[i].second;
         }
-        ll ans = 2e18;
-        while(s<=e)
+        
+        // prefix sum of cost 
+        for(int i=1;i<n;i++)
         {
-            int m = (s+e)>>1;
-            ll curr = f(m,nums,cost);
-            ll left = f(m-1,nums,cost);
-            ll right = f(m+1,nums,cost);
-            ans = min(ans,curr);
-            if(curr>right) s = m+1;
-            else if(curr>left) e = m-1;
-            else return curr;
+            v[i].second += v[i-1].second;
+        }
+        
+        // minimizing the ans
+        for(int i=1;i<n;i++)
+        {
+            ll dif = v[i].first - v[i-1].first;
+            ll left = (v[i-1].second)*dif;
+            ll right = (v[n-1].second-v[i-1].second)*dif;
+            ans = min(ans,ans+left-right);
         }
         return ans;
     }
