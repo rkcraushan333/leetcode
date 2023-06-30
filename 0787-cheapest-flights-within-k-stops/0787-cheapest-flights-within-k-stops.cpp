78ee85b87304c7e212afd82fd0f816f30b44cc8b
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-06-30 20:41:40 +0530

    Time: 26 ms (64.56%), Space: 13.7 MB (28.29%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,16 +1,43 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& v, int src, int dst, int k) {
-        vector<int>dis(n,1e8);
-        dis[src] = 0;
-        for(int i=0;i<=k;i++){
-            vector<int>c(dis);
-            for(auto e:v){
-                c[e[1]] = min(c[e[1]],dis[e[0]]+e[2]);
+    #define ppi pair<int,pair<int,int>>
+    vector<int> dj(vector<vector<pair<int,int>>>&adj,int src,int n,int k)
+    {
+        vector<int>v(n,INT_MAX);
+        v[src] = 0;
+        queue<ppi>pq;
+        pq.push({0,{src,0}});
+        while(!pq.empty())
+        {
+            auto t = pq.front(); pq.pop();
+            int pr = t.first;
+            int node = t.second.first;
+            int k1 = t.second.second;
+            if(k1>k) 
+            {
+                continue;
             }
-            dis = c;
+            for(auto x:adj[node])
+            {
+                int nd = x.first,pr1 = x.second;
+                if(v[nd]>pr1+pr)
+                {
+                    v[nd] = pr1+pr;
+                    pq.push({pr1+pr,{nd,k1+1}});
+                }
+            }
         }
-        if(dis[dst]==1e8) return -1;
-        return dis[dst];
+        return v;
+    }
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) 
+    {
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto v:flights)
+        {
+            adj[v[0]].push_back({v[1],v[2]});
+        }
+        vector<int>v1 = dj(adj,src,n,k);
+        // for(auto i:v1) cout<<i<<" ";
+        return v1[dst]==INT_MAX?-1:v1[dst];
     }
 };
