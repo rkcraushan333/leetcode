b94aa508fa1dc78abd1a9dece6ba3ff76125205e
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-07-04 09:53:46 +0530

    Time: 15 ms (16.54%), Space: 9.4 MB (72.62%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
-    int singleNumber(vector<int>& nums) 
-    {
-        unordered_map<int,int>m;
-        for(auto i:nums) m[i]++;
-        for(auto i:m)
+    // count each bit 
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int j=0;j<32;j++)
         {
-            if(i.second==1) return i.first;
+            int cnt = 0;
+            for(auto i:nums)
+            {
+                if(i&(1<<j)) cnt++;
+            }
+            if(cnt%3) ans |= 1<<j;
         }
-        return 0;
+        return ans;
     }
 };
