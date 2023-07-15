725859149391c2463f533f24496479308032de2d
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-07-15 22:17:52 +0530

    Time: 51 ms (23.46%), Space: 7.2 MB (85.02%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
     int dp[501][501];
-    int f(string &s, string &t,int i=0,int j=0){
-        if(i==s.size()&&j==t.size()) return 0;
+    int f(string &s,string &t,int i=0,int j=0)
+    {
         if(i==s.size()) return t.size()-j;
         if(j==t.size()) return s.size()-i;
-        int &tt = dp[i][j];
-        if(tt!=-1) return tt;
-        int res = 1e7;
+        int &rkc = dp[i][j];
+        if(rkc!=-1) return rkc;
+        int ans = 1e8;
+        // if same 
         if(s[i]==t[j])
-            res = min(res,f(s,t,i+1,j+1));
-        else{
-            res = min(res,1+f(s,t,i+1,j+1));
-            res = min(res,1+f(s,t,i,j+1));
-            res = min(res,1+f(s,t,i+1,j));
+        {
+            ans = min(ans,f(s,t,i+1,j+1));
         }
-        return tt = res;
+        else
+        {
+            // insert in 1st
+            ans = min(ans,1+f(s,t,i,j+1));
+            // insert in 2nd
+            ans = min(ans,1+f(s,t,i+1,j));
+            // delete in 1st 
+            // ans = min(ans,1+f(s,t,i+1,j));
+            // // delete in 2nd
+            // ans = min(ans,1+f(s,t,i,j+1));
+            // // replace in 1st
+            // ans = min(ans,1+f(s,t,i+1,j+1));
+            // replace in 2nd 
+            ans = min(ans,1+f(s,t,i+1,j+1));
+        }
+        return rkc = ans;
     }
-    int minDistance(string &s, string &t) {
+    int minDistance(string &s, string &t) 
+    {
         memset(dp,-1,sizeof(dp));
         return f(s,t);
     }
