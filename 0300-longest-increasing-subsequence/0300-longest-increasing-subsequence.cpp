c26366e304dc252a39f93fe753cabda3237bd61f
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-07-16 23:58:37 +0530

    Time: 1142 ms (25.79%), Space: 38 MB (36.25%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,14 +1,34 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        vector<int>v;
-        for(auto i:nums){
-            int t = lower_bound(v.begin(),v.end(),i)-v.begin();
-            if(t==v.size())
-                v.push_back(i);
-            else
-                v[t] = i;
+    int dp[2501][2502];
+    int f(vector<int>&v,int i=0,int pre=-1)
+    {
+        if(i==v.size()) return 0;
+        int &rkc = dp[i][pre+1];
+        if(rkc!=-1) return rkc;
+        int ans = 0;
+        if(pre==-1)
+        {
+            ans = max(ans,1+f(v,i+1,i));
+            ans = max(ans,f(v,i+1,pre));
         }
-        return v.size();
+        else
+        {
+            if(v[i]>v[pre])
+            {
+                ans = max(ans,1+f(v,i+1,i));
+                ans = max(ans,f(v,i+1,pre));
+            }
+            else
+            {
+                ans = max(ans,f(v,i+1,pre));
+            }
+        }
+        return rkc = ans;
+    }
+    int lengthOfLIS(vector<int>& nums)
+    {
+        memset(dp,-1,sizeof(dp));
+        return f(nums);
     }
 };
