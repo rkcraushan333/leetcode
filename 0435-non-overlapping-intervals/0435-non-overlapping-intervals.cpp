397f39fd86fd49739d2455af31fd69a56eb1c376
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-07-19 22:01:27 +0530

    Time: 483 ms (66.32%), Space: 89.8 MB (58.83%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int dp[100001];
-    vector<int>v1;
-    int f(vector<vector<int>>&v,int i=0)
+    static bool cmp(vector<int>&a,vector<int>&b)
     {
-        if(i==v.size()) return 0;
-        int &rkc = dp[i];
-        if(rkc!=-1) return rkc;
-        int ans = 1e9;
-        ans = min(ans,1+f(v,i+1));
-        int t = lower_bound(v1.begin(),v1.end(),v[i][1])-v1.begin();
-        ans = min(ans,t-i-1+f(v,t));
-        return rkc = ans;
+        return a[1]<b[1];
     }
     int eraseOverlapIntervals(vector<vector<int>>& v) 
     {
-        memset(dp,-1,sizeof(dp));
-        sort(v.begin(),v.end());
-        for(int i=0;i<v.size();i++)
+        sort(v.begin(),v.end(),cmp);
+        int ans = 0;
+        int l = v[0][1];
+        for(int i=1;i<v.size();i++)
         {
-            v1.push_back(v[i][0]);
+            if(v[i][0]<l) ans++;
+            else
+            {
+                l = v[i][1];
+            }
         }
-        return f(v);
+        return ans;
     }
 };
