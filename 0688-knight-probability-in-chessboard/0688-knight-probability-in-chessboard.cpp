a1acdbca0cb892193009095beec0a4903685c3ab
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-07-22 17:22:29 +0530

    Time: 5 ms (95.48%), Space: 6.3 MB (98.38%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int dx[8] = {-2,-1,1,2,2,1,-1,-2};
-    int dy[8] = {1,2,2,1,-1,-2,-2,-1};
-    double dp[101][26][26];
-    double f(int n,int k,int r,int c){
-        if(r<0||c<0||r>=n||c>=n)
-            return 0.0;
+    double dp[25][25][101];
+    int dx[8] = {1,1,-1,-1,2,2,-2,-2};
+    int dy[8] = {2,-2,2,-2,1,-1,-1,1};
+    double f(int i,int j,int n,int k)
+    {
+        if(i<0||j<0||i>=n||j>=n) return 0;
         if(k==0) return 1;
-        if(dp[k][r][c]!=0.0)
-            return dp[k][r][c];
-        double ans = 0.0;
-        for(int i=0;i<8;i++)
+        double &t = dp[i][j][k];
+        if(t!=0) return t;
+        double ans=0;
+        for(int x=0;x<8;x++)
         {
-            ans += f(n,k-1,r+dx[i],c+dy[i]);
+            ans += f(i+dx[x],j+dy[x],n,k-1)/8;
         }
-        return dp[k][r][c]=ans/8;
+        return t=ans;
     }
-    double knightProbability(int n, int k, int r, int c) {
+    double knightProbability(int n, int k, int r, int c) 
+    {
         memset(dp,0.0,sizeof(dp));
-        return f(n,k,r,c);
+        return f(r,c,n,k);
     }
 };
