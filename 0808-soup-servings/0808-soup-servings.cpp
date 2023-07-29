61b174e142964534b400588d352b5ff35996da07
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-07-29 23:27:51 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (72.62%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.cpp
+++ 0808-soup-servings/0808-soup-servings.cpp
@@ -1,19 +1,28 @@
 class Solution {
 public:
-    vector<vector<double>>dp;
-    double f(int a,int b){
-        if(a<=0 && b<=0) 
-            return 0.5;
-        if(a<=0)
-            return 1.0;
-        if(b<=0) 
-            return 0.0;
-        if(dp[a][b]!=-1) return dp[a][b];
-        return dp[a][b]=(f(a-100,b)+f(a-75,b-25)+f(a-50,b-50)+f(a-25,b-75))/4;
+    map<pair<int,int>,double>m;
+    double f(int a,int b)
+    {
+        if(a<=0) 
+        {
+            if(b<=0) return 0.5;
+            return 1;
+        }
+        if(b<=0) return 0;
+        if(m.count({a,b}))
+        {
+            return m[{a,b}];
+        }
+        double ans = 0;
+        ans += f(a-100,b)/4.0;
+        ans += f(a-75,b-25)/4.0;
+        ans += f(a-50,b-50)/4.0;
+        ans += f(a-25,b-75)/4.0;
+        return m[{a,b}] = ans;
     }
-    double soupServings(int n) {
+    double soupServings(int n) 
+    {
         if(n>4800) return 1;
-        dp.resize(n+1,vector<double>(n+1,-1));
         return f(n,n);
     }
 };
