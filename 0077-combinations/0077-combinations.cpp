347b627a351a9a9b2614ae4d0203f92623cc67b6
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-08-01 12:34:39 +0530

    Time: 25 ms (79.09%), Space: 18.8 MB (24.27%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     vector<vector<int>>res;
-    void f(int n,int k,int i,vector<int>&v){
-        if(v.size()==k){
-            res.push_back(v);
+    void f(int n,int k,vector<int>&v,int i=1)
+    {
+        if(i==n+1)
+        {
+            if(v.size()==k) res.push_back(v);
             return;
         }
-        if(i==n+1) return;
-        f(n,k,i+1,v);
-        if(v.size()>0){
-            if(i>v[v.size()-1])
-            {
-                v.push_back(i);
-                f(n,k,i+1,v);
-                v.pop_back();
-            }
-        }else{
-            v.push_back(i);
-            f(n,k,i+1,v);
-            v.pop_back();
-        }
+        f(n,k,v,i+1);
+        v.push_back(i);
+        f(n,k,v,i+1);
+        v.pop_back();
     }
-    vector<vector<int>> combine(int n, int k) {
+    vector<vector<int>> combine(int n, int k) 
+    {
         vector<int>v;
-        f(n,k,1,v);
+        f(n,k,v);
         return res;
     }
 };
