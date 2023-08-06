6150b41a703a726959f6b26a96e27e7ef6fc3bb1
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-08-06 23:56:36 +0530

    Time: 12 ms (18.02%), Space: 14 MB (5.86%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -1,18 +1,27 @@
+#define ll long long
+const ll mod = 1e9+7;
+ll dp[101][101][101];
 class Solution {
 public:
-    #define ll long long
-    const int MOD = 1e9 + 7;
-    ll solve(int n, int goal, int k, vector<vector<int>>& dp) {
-        if (n == 0 && goal == 0) return 1;
-        if (n == 0 || goal == 0) return 0;
-        if (dp[n][goal] != -1) return dp[n][goal];
-        ll pick = solve(n - 1, goal - 1, k, dp) * n;
-        ll notpick = solve(n, goal - 1, k, dp) * max(n - k, 0);
-        return dp[n][goal] = (pick + notpick) % MOD;
+    ll f(int n,int t,int goal,int k)
+    {
+        if(goal==0) return n==0;
+        ll &rkc = dp[n][goal][t];
+        if(rkc!=-1) return rkc;
+        ll ans = 0;
+        if(n>0)
+        {
+            ans += f(n-1,t,goal-1,k)*n; ans %= mod;
+        }
+        if(t-n>k)
+        {
+            ans += f(n,t,goal-1,k)*(t-n-k); ans %= mod;
+        }
+        return rkc = ans;
     }
-
-    int numMusicPlaylists(int n, int goal, int k) {
-        vector<vector<int>> dp(n + 1, vector<int>(goal + 1, -1));
-        return solve(n, goal, k, dp);
+    int numMusicPlaylists(int n, int goal, int k) 
+    {
+        memset(dp,-1,sizeof(dp));
+        return f(n,n,goal,k);
     }
 };
