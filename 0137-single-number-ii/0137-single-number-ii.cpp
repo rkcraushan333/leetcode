34f83b0a11d8bc379d8b4c4f6e190dcb2f0d26b6
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-08-14 02:01:17 +0530

    Time: 10 ms (49.30%), Space: 9.5 MB (40.98%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    // count each bit 
-    int singleNumber(vector<int>& nums) {
+    int singleNumber(vector<int>& nums) 
+    {
         int ans = 0;
         for(int j=0;j<32;j++)
         {
             int cnt = 0;
             for(auto i:nums)
             {
-                if(i&(1<<j)) cnt++;
+                if((i>>j)&1)
+                {
+                    cnt++;
+                }
             }
-            if(cnt%3) ans |= 1<<j;
+            if(cnt%3!=0)
+            {
+                ans |= (1<<j);
+            }
         }
         return ans;
     }
