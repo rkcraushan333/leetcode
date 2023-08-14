0128c367056d037715a307cee12c7fea8c6b96dc
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-08-14 17:49:01 +0530

    Time: 86 ms (87.32%), Space: 47.3 MB (49.46%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,17 +1,68 @@
+class Minheap{
+private:
+    int sz;
+    vector<int>v;
+public:
+    Minheap(int n)
+    {
+        sz = 0;
+        v.resize(n+1);
+    }
+    void Push(int val)
+    {
+        sz++;
+        v[sz] = val;
+        int last = sz;
+        while(last>1)
+        {
+            if(v[last]<v[last/2])
+            {
+                swap(v[last],v[last/2]);
+                last /= 2;
+            }
+            else break;
+        }
+    }
+    void Pop()
+    {
+        swap(v[1],v[sz]);
+        sz--;
+        int curr = 1;
+        while(curr<sz)
+        {
+            int l = 2*curr;
+            int r = 2*curr+1;
+            int c = curr;
+            if(l<=sz&&v[c]>v[l]) c = l;
+            if(r<=sz&&v[c]>v[r]) c = r;
+            if(c==curr) break;
+            swap(v[c],v[curr]);
+            curr = c;
+        }
+    }
+    int Size()
+    {
+        return sz;
+    }
+    int Top()
+    {
+        return v[1];
+    }
+};
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) 
     {
-        priority_queue<int,vector<int>,greater<int>>pq;
+        Minheap pq(nums.size());
         for(auto i:nums)
         {
-            if(pq.size()<k) pq.push(i);
-            else if(pq.top()<i) 
+            if(pq.Size()<k) pq.Push(i);
+            else if(pq.Top()<i)
             {
-                pq.pop();
-                pq.push(i);
+                pq.Pop();
+                pq.Push(i);
             }
         }
-        return pq.top();
+        return pq.Top();
     }
 };
