45b77f7319906d1f482d0f943d069664ef06d0bf
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-09-17 21:53:36 +0530

    Time: 202 ms (68.55%), Space: 68.3 MB (48.26%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.cpp
+++ 1462-course-schedule-iv/1462-course-schedule-iv.cpp
@@ -1,32 +1,42 @@
+// Toposort Best way
 class Solution {
 public:
-    vector<vector<int>>adj;
-    bool dfs(int s,int l,vector<bool>&vis)
-    {
-        if(s==l) return true;
-        vis[s] = 1;
-        bool ans = 0;
-        for(auto child:adj[s])
-        {
-            if(!vis[child])
-            {
-                ans |= dfs(child,l,vis);
-            }
-        }
-        return ans;
-    }
     vector<bool> checkIfPrerequisite(int n, vector<vector<int>>&p, vector<vector<int>>&q) 
     {
-        adj.resize(n);
+        vector<vector<int>>adj(n);
+        vector<int>indeg(n);
         for(int i=0;i<p.size();i++)
         {
             adj[p[i][0]].push_back(p[i][1]);
+            indeg[p[i][1]]++;
+        }
+        set<int>st[n];
+        queue<int>q1;
+        for(int i=0;i<n;i++)
+        {
+            if(indeg[i]==0) q1.push(i);
+        }
+        while(q1.size())
+        {
+            int t = q1.front(); q1.pop();
+            for(auto child:adj[t])
+            {
+                st[child].insert(t);
+                for(auto i:st[t])
+                {
+                     st[child].insert(i);
+                 }
+                indeg[child]--;
+                if(indeg[child]==0)
+                {
+                    q1.push(child);
+                }
+            }
         }
         vector<bool>ans;
         for(int i=0;i<q.size();i++)
         {
-            vector<bool>vis(n);
-            ans.push_back(dfs(q[i][0],q[i][1],vis));
+            ans.push_back(st[q[i][1]].count(q[i][0]));
         }
         return ans;
     }
