bb44d2138cb3a2fb9b40bb68560627a88b58e825
RAUSHAN KUMAR <76718202+rkcraushan333@users.noreply.github.com>
2023-09-19 21:23:05 +0530

    Time: 172 ms (45.51%), Space: 70.7 MB (8.10%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,25 +1,40 @@
-// O(2n) == O(n)
+// Using DSU
 class Solution {
 public:
+    vector<int>par,Size;
+    int finds(int a)
+    {
+        return par[a] = (a==par[a]?a:finds(par[a]));
+    }
+    void unions(int a,int b)
+    {
+        a = finds(a);
+        b = finds(b);
+        if(a!=b)
+        {
+            if(Size[a]>Size[b]) swap(a,b);
+            Size[a] += Size[b];
+            par[b] = a;
+        }
+    }
     int longestConsecutive(vector<int>& nums) 
     {
-        unordered_set<int>s;
-        for(auto i:nums) s.insert(i);
-        int ans = 0;
-        for(auto i:nums)
+        unordered_map<int,int>mp;
+        int n = nums.size();
+        par.resize(n);
+        Size.resize(n,1);
+        for(int i=0;i<n;i++) par[i] = i;
+        for(int i=0;i<n;i++)
         {
-            int curr =  i;
-            if(s.find(curr-1)==s.end())
-            {
-                int temp = 0;
-                while(s.find(curr)!=s.end())
-                {
-                    curr++;
-                    temp++;
-                }
-                ans = max(ans,temp);
-            }
+            int curr = nums[i];
+            // Here we will not include those no which allreay been used
+            if(mp.count(curr)) continue;
+            if(mp.count(curr-1)) unions(i,mp[curr-1]);
+            if(mp.count(curr+1)) unions(i,mp[curr+1]);
+            mp[curr] = i;
         }
+        int ans = 0;
+        for(int i=0;i<n;i++) ans = max(ans,Size[i]);
         return ans;
     }
 };
